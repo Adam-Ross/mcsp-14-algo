f4d921c419eab0c5e959e4a386927c204b1c81a1
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-08-29 10:27:00 -0500

    Fixed bug.

---
+++ validInput/main.js
@@ -0,0 +1,11 @@
+// assume you will take in a string
+
+// '()' -> true, the number of opening and closing parens are balanced
+// '(())' -> tue
+// ')()' -> false - the opening and closing parens are not balanced.
+// ')(' -> false
+
+function validInput(str) {
+    // should return a true or false depending on if the str is balanced.
+}
+
