735c5c7f16b83b7d27d4b8eb4d2f92516f607e7e
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-09-02 11:07:04 -0500

    finzzBuzzObject

--- fizzBuzzObj/fizzBuzzObj.js
+++ fizzBuzzObj/fizzBuzzObj.js
@@ -1,18 +1,72 @@
-// % 3 === fizz
-// % 5 === buzz
-// % 5 && 3 === fizzBuzz
+// // % 3 === fizz
+// // % 5 === buzz
+// // % 5 && 3 === fizzBuzz
 
-// Write and algo that tells me the number of fizzBuzzes between 1 and n.
-// this shoult tell me the NUMBER of fizzbuzzes and give me an optional
-// object showing the formatted fizzes, buzzes, and fizzBuzzes
+// // Write and algo that tells me the number of fizzBuzzes between 1 and n.
+// // this shoult tell me the NUMBER of fizzbuzzes and give me an optional
+// // object showing the formatted fizzes, buzzes, and fizzBuzzes
 
-// n = 15 => 1
+// // n = 15 => 1
 
 
-// `There is 1 fizzBuzz between 1 and 15. Further data here: 
-    // {
-    // fizz: 4,
-    // buzz: 2, 
-    // fizzBuzz: 1
-// }
+// // `There is 1 fizzBuzz between 1 and 15. Further data here: 
+//     // {
+//     // fizz: 4,
+//     // buzz: 2, 
+//     // fizzBuzz: 1
+// // }
+
+
+function fizzBuzzObj(num) {
+    const data = {}
+    let i = 1;
+    while(i <= num) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            data.fizzBuzz ? data.fizzBuzz++ : data.fizzBuzz = 1
+        } else if(i % 3 === 0) {
+            data.fizz ? data.fizz++ : data.fizz = 1
+        } else if(i % 5 === 0) {
+            data.buzz ? data.buzz++ : data.buzz = 1
+        } 
+        i++
+    }
+    
+    return `There are ${data.fizzBuzz} fizzBuzzes between 1 and ${num}. Further data here: ${JSON.stringify(data)}`
+}
+
+console.log(fizzBuzzObj(15))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
